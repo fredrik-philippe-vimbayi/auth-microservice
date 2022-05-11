34fc0bcc9123e73139dd336cb64157beb683796e
Fredrik <f.erikzzon@gmail.com>
2022-05-11 22:19:14 +0200

    Adds AuthFilter

--- src/main/java/com/example/authservice/auth/JWTAuthenticationFilter.java
+++ src/main/java/com/example/authservice/auth/JWTAuthenticationFilter.java
@@ -1,62 +1,52 @@
 package com.example.authservice.auth;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.example.authservice.entity.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import static com.example.authservice.auth.SecurityConstants.HEADER_STRING;
+import static com.example.authservice.auth.SecurityConstants.TOKEN_PREFIX;
 
-import static com.example.authservice.auth.SecurityConstants.EXPIRATION_TIME;
-import static com.example.authservice.auth.SecurityConstants.SECRET;
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
-public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    private AuthenticationManager authenticationManager;
-
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-
-        setFilterProcessesUrl("api/services/controller/user/login");
-    }
+    @Autowired
+    private TokenProvider jwtTokenUtil;
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-        try {
-            User creds = new ObjectMapper()
-                    .readValue(request.getInputStream(), User.class);
-
-            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(creds.getPassword(), creds.getEmail(), new ArrayList<>()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader(HEADER_STRING);
+        String username = null;
+        String authToken = null;
+        if (header != null && header.startsWith(TOKEN_PREFIX)) {
+            authToken = header.replace(TOKEN_PREFIX, "");
+            try {
+                username = jwtTokenUtil.getUsernameFromToken(authToken);
+            } catch (IllegalArgumentException e) {
+                logger.error("An error occurred while fetching Username from Token", e);
+            } catch (ExpiredJwtException e) {
+                logger.warn("The token has expired", e);
+            } catch (SignatureException e) {
+                logger.error("Authentication Failed. Username or Password not valid.");
+            }
+        } else {
+            logger.warn("Couldn't find bearer string, header will be ignored");
         }
-    }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication auth) throws IOException{
+            UsernamePasswordAuthenticationToken authentication = jwtTokenUtil.getAuthenticationToken(authToken, SecurityContextHolder.getContext().getAuthentication());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            logger.info("authenticated user " + username + ", setting security context");
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
-        String token = JWT.create()
-                .withSubject(((User) auth.getPrincipal()).getEmail())
-                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .sign(Algorithm.HMAC512(SECRET.getBytes()));
-
-        String body = ((User) auth.getPrincipal()).getEmail() + " " + token;
-
-        response.getWriter().write(body);
-        response.getWriter().flush();
+        filterChain.doFilter(request, response);
     }
 }
