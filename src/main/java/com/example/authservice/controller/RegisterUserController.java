e845351d5715bc2594f405e42e413d8670b8b885
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-12 22:56:46 +0200

    Add username exists validation

--- src/main/java/com/example/authservice/controller/RegisterUserController.java
+++ src/main/java/com/example/authservice/controller/RegisterUserController.java
@@ -2,25 +2,31 @@ package com.example.authservice.controller;
 
 import com.example.authservice.dto.UserDto;
 import com.example.authservice.service.UserService;
+import com.example.authservice.validator.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("register")
 public class RegisterUserController {
 
     private final UserService userService;
+    private final UserValidator userValidator;
 
-    public RegisterUserController(UserService userService) {
+    public RegisterUserController(UserService userService, UserValidator userValidator) {
         this.userService = userService;
+        this.userValidator = userValidator;
     }
 
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody UserDto user) {
+        if (userValidator.userExists(user.username()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username already exists");
         userService.createUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
