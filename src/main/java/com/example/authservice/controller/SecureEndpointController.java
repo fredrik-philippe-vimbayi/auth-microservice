91fdb4698a7b175e8cbd7271555692479c417dc3
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-12 10:00:26 +0200

    Add HTTP method annotation

--- src/main/java/com/example/authservice/controller/SecureEndpointController.java
+++ src/main/java/com/example/authservice/controller/SecureEndpointController.java
@@ -3,6 +3,7 @@ package com.example.authservice.controller;
 import com.example.authservice.dto.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class SecureEndpointController {
 
+    @GetMapping
     public ResponseEntity<Message> getMessage() {
-        Message message = new Message("You have reached a secured endpoint.");
+        Message message = new Message("You have reached a secure endpoint.");
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
