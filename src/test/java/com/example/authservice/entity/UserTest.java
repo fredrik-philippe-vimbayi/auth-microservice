27173da179b2061f38c8f63be25d20ab70049f60
Philippe <philippe.vial90@gmail.com>
2022-05-12 10:37:10 +0200

    Add tests for field annotation on password

--- src/test/java/com/example/authservice/entity/UserTest.java
+++ src/test/java/com/example/authservice/entity/UserTest.java
@@ -3,6 +3,12 @@ package com.example.authservice.entity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
@@ -11,12 +17,17 @@ class UserTest {
 
     private User user;
 
+    private Validator validator;
+
     @BeforeEach
     public void init() {
         role = new Role("user");
         user = new User("angela@mymail.com", "angels@25");
 
         user.addRole(role);
+
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     }
 
     @Test
@@ -36,5 +47,32 @@ class UserTest {
         var roles = user.getRoles();
 
         assertThat(roles).isEmpty();
+    }
+
+    @Test
+    void whenNotEmptyPasswordThenNoConstraintViolations() {
+        user.setPassword("password");
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenEmptyPasswordThenTwoConstraintViolations() {
+        user.setPassword("");
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        assertThat(violations).hasSize(2);
+    }
+
+    @Test
+    void whenNullPasswordThenOneConstraintViolations() {
+        user.setPassword(null);
+
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        assertThat(violations).hasSize(1);
     }
 }
