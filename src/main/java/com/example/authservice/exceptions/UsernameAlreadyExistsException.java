6f6213c93c64b188d418fe4f6b87f80154359411
Philippe <philippe.vial90@gmail.com>
2022-05-13 13:35:39 +0200

    Rename NotUniqueEmailException.java to UsernameAlreadyExistsException.java

--- src/main/java/com/example/authservice/exceptions/UsernameAlreadyExistsException.java
+++ src/main/java/com/example/authservice/exceptions/UsernameAlreadyExistsException.java
@@ -2,11 +2,11 @@ package com.example.authservice.exceptions;
 
 import org.springframework.validation.BindingResult;
 
-public class NotUniqueEmailException extends RuntimeException{
+public class UsernameAlreadyExistsException extends RuntimeException{
 
     private final transient BindingResult errors;
 
-    public NotUniqueEmailException(String message, BindingResult errors) {
+    public UsernameAlreadyExistsException(String message, BindingResult errors) {
         super(message);
         this.errors = errors;
     }
