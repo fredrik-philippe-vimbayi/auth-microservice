b961865526e9162d63f7be4be249d8af0a718543
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-13 11:41:05 +0200

    Remove unused constants

--- src/main/java/com/example/authservice/security/SecurityConstants.java
+++ src/main/java/com/example/authservice/security/SecurityConstants.java
@@ -1,17 +1,8 @@
 package com.example.authservice.security;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class SecurityConstants {
-
-    public static final String PUBLIC_KEY = getPublicKey();
-    public static final String HEADER_NAME = "Authorization";
+    
     public static final String TOKEN_PREFIX = "Bearer";
     public static final Long EXPIRATION_TIME = 2 * 60 * 60 * 10L;
-
-    private static String getPublicKey() {
-        Dotenv dotenv = Dotenv.configure().load();
-        return dotenv.get("PUBLIC_KEY");
-    }
 
 }
