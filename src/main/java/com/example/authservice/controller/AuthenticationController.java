4a11c7686e5b67083b77d4317a247dc7fba3bfe2
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-13 11:09:53 +0200

    Refactor AuthenticationController

--- src/main/java/com/example/authservice/controller/AuthenticationController.java
+++ src/main/java/com/example/authservice/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.example.authservice.dto.UserDto;
 import com.example.authservice.entity.User;
 import com.example.authservice.repository.UserRepository;
 import com.example.authservice.service.TokenService;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,32 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("authentication")
+@RequestMapping("authenticate")
 public class AuthenticationController {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenService tokenService;
-
+    private final Logger logger;
 
     public AuthenticationController(PasswordEncoder passwordEncoder, UserRepository userRepository,
-                                    TokenService tokenService) {
+                                    TokenService tokenService, Logger logger) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.tokenService = tokenService;
+        this.logger = logger;
     }
 
     @PostMapping
     public ResponseEntity<Token> authenticate(@RequestBody UserDto userDto) {
         User user = userRepository.findByUsername(userDto.username());
 
-        if (user == null)
+        if (user == null) {
+            logger.warn("Unsuccessful login - username not found");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Incorrect login credentials");
-
+        }
+        
         if (passwordEncoder.matches(userDto.password(), user.getPassword())) {
             Token token = tokenService.generateToken(user);
             return new ResponseEntity<>(token, HttpStatus.OK);
         } else {
+            logger.warn("Unsuccessful login attempt for userId {}", user.getId());
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Incorrect login credentials");
         }
     }
