a76575d23f1730536effefc3c88512a485bf8487
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-13 09:03:21 +0200

    Fix  error decoding key

--- src/main/java/com/example/authservice/service/KeyService.java
+++ src/main/java/com/example/authservice/service/KeyService.java
@@ -1,13 +1,13 @@
 package com.example.authservice.service;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.io.Decoders;
 
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.util.Base64;
 
 public class KeyService {
 
@@ -15,7 +15,7 @@ public class KeyService {
 
     public PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
         String key = dotenv.get("PRIVATE_KEY");
-        byte[] keyBytes = Base64.getDecoder().decode(key);
+        byte[] keyBytes = Decoders.BASE64.decode(key);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePrivate(keySpec);
