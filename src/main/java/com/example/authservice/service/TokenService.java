7978a07cc81d8ae89f68b0ebb324b936020ea549
Fredrik <f.erikzzon@gmail.com>
2022-05-16 16:01:55 +0200

    Add dependency and added rabbitMQ address to application.properties

--- src/main/java/com/example/authservice/service/TokenService.java
+++ src/main/java/com/example/authservice/service/TokenService.java
@@ -4,6 +4,7 @@ import com.example.authservice.dto.Token;
 import com.example.authservice.entity.Role;
 import com.example.authservice.entity.User;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataRetrievalFailureException;
 
@@ -36,7 +37,7 @@ public class TokenService {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(expiration)
                 .claim("roles", roles)
-                .signWith(key)
+                .signWith(SignatureAlgorithm.RS256,key)
                 .compact();
 
         return new Token(jwtToken, TOKEN_PREFIX, EXPIRATION_TIME);
