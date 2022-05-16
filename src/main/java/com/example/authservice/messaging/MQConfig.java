3d80909f303fc516c119ac27e6ca17543b403e23
Fredrik <f.erikzzon@gmail.com>
2022-05-16 16:02:27 +0200

    Add MQConfig

--- src/main/java/com/example/authservice/messaging/MQConfig.java
+++ src/main/java/com/example/authservice/messaging/MQConfig.java
@@ -1,7 +1,10 @@
 package com.example.authservice.messaging;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,7 @@ public class MQConfig {
 
     public static final String QUEUE = "message_queue";
     public static final String EXCHANGE = "message_exchange";
+    public static final String ROUTING_KEY = "message_exchange";
 
     @Bean
     public Queue queue(){
@@ -20,5 +24,26 @@ public class MQConfig {
     public TopicExchange exchange (){
         return new TopicExchange(EXCHANGE);
     }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange){
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
 
 }
