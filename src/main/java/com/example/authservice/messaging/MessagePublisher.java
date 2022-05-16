1b647e00f3ae863627bced90c6b18e09482386e2
Fredrik <f.erikzzon@gmail.com>
2022-05-16 17:15:24 +0200

    minor updates to publishMessage-method, messaage is now added to queue when creating user

--- src/main/java/com/example/authservice/messaging/MessagePublisher.java
+++ src/main/java/com/example/authservice/messaging/MessagePublisher.java
@@ -3,7 +3,6 @@ package com.example.authservice.messaging;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +15,12 @@ public class MessagePublisher {
     @Autowired
     private RabbitTemplate template;
 
-    @PostMapping("/publish")
-    public String publishMessage(@RequestBody CustomMessage message){
-        message.setMessageId(UUID.randomUUID().toString());
-        message.setMessageDate(new Date());
-        template.convertAndSend(MQConfig.EXCHANGE,MQConfig.ROUTING_KEY, message);
+    public void publishMessage(@RequestBody String username){
 
-        return "Message Published";
+        CustomMessage customMessage = new CustomMessage(UUID.randomUUID().toString(),
+                "User created: " + username, new Date());
+
+        template.convertAndSend(MQConfig.EXCHANGE,MQConfig.ROUTING_KEY, customMessage);
 
     }
 }
