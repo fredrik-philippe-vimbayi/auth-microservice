ec9ed65d149d8718e7af84a8cd72b80eb394d800
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-16 15:27:09 +0200

    Refactor LoggerConfig

--- src/main/java/com/example/authservice/beans/LoggerConfig.java
+++ src/main/java/com/example/authservice/beans/LoggerConfig.java
@@ -2,14 +2,17 @@ package com.example.authservice.beans;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class LoggerConfig {
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public Logger logger() {
-        return LogManager.getLogger("AUTH");
+        return LogManager.getLogger("AUTHENTICATION");
     }
 }
