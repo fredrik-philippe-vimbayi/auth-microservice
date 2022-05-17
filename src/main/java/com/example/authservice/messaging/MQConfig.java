de746b6d0e00329909922553841ee000d8099168
Fredrik <f.erikzzon@gmail.com>
2022-05-17 10:39:24 +0200

    removed unused message Queue

--- src/main/java/com/example/authservice/messaging/MQConfig.java
+++ src/main/java/com/example/authservice/messaging/MQConfig.java
@@ -11,26 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MQConfig {
 
-    public static final String QUEUE = "message_queue";
     public static final String EXCHANGE = "message_exchange";
     public static final String ROUTING_KEY = "message_exchange";
 
     @Bean
-    public Queue queue(){
-        return new Queue(QUEUE);
-    }
-
-    @Bean
     public TopicExchange exchange (){
         return new TopicExchange(EXCHANGE);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange){
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(ROUTING_KEY);
     }
 
     @Bean
