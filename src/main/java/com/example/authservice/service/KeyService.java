c607ad713d7791e8479d3c3c0a610ddd6aed388b
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-17 19:31:48 +0200

    Refactor key-pair to consul config

--- src/main/java/com/example/authservice/service/KeyService.java
+++ src/main/java/com/example/authservice/service/KeyService.java
@@ -1,7 +1,7 @@
 package com.example.authservice.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.io.Decoders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.KeyFactory;
@@ -9,14 +9,16 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+
 @Service
 public class KeyService {
 
-    private static final Dotenv dotenv = Dotenv.configure().load();
+
+    @Value("${key.private}")
+    private String privateKey;
 
     public PrivateKey getPrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
-        String key = dotenv.get("PRIVATE_KEY");
-        byte[] keyBytes = Decoders.BASE64.decode(key);
+        byte[] keyBytes = Decoders.BASE64.decode(privateKey);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         return keyFactory.generatePrivate(keySpec);
